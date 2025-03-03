66a17e6d2323e6f1943fba24574125595604ab53
Alex Kulik <bubralubra@gmail.com>
2025-03-04 01:58:30 +0300

    feature: made cool search input toggler

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,100 +1,65 @@
 import { useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
-// import {
-//   selectGamesSearchHistory,
-//   selectFilmsSearchHistory
-// } from "../../selectors/selectors";
+import { FaGamepad, FaFilm, FaSearch } from "react-icons/fa";
 import classes from "./SearchBar.module.scss";
 import { INSCRIPTIONS_KEYS, SEARCH_TYPE } from "../../constants/constants";
-import { fetchGamesListbyName, fetchFilmsListbyName, storeFilmSearchList, storeGameSearchList } from "../../actions";
+import {
+  fetchFilmsListbyName,
+} from "../../actions";
 import { getListByName } from "../../reducers/searchReducer";
 
 export default function SearchBar() {
-  // const gamesSearchHistory = useSelector(selectGamesSearchHistory);
-  // const filmsSearchHistory = useSelector(selectFilmsSearchHistory);
-  const [searchBoxClass, setSearchBoxClass] = useState("");
-  const [searchType, setSearchType] = useState(SEARCH_TYPE.GAMES);
+  const [isMoviesSearch, setisMoviesSearch] = useState(false);
   const inputRef = useRef(null);
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  const handleSubmit = async (useKeyboard) => {
-    const searchItem = inputRef.current.value;
-    if (!searchItem) return;
-//TODO Make dependand of API request and store loading state
-    inputRef.current.value = "";
+  const handleSubmit = async (event, useKeyboard) => {
+    const searchItem = inputRef.current.value.trim();
 
-    if (useKeyboard) {
-      navigate("/results");
-    }
-    inputRef.current.disabled = true;
+    if (!searchItem) return event.preventDefault();
+    if (useKeyboard) navigate("/results");
 
-    // if (searchType === SEARCH_TYPE.FILMS) {
-    //   if (searchItem in filmsSearchHistory) {
-    //     const list = { [searchItem]: filmsSearchHistory[searchItem] };
-    //     inputRef.current.disabled = false;
-    //     return dispatch(storeFilmSearchList(list));
-    //   }
+    isMoviesSearch
+      ? dispatch(fetchFilmsListbyName(searchItem))
+      : dispatch(getListByName(searchItem));
+  };
 
-    //   inputRef.current.disabled = false;
-    //   return dispatch(fetchFilmsListbyName(searchItem))
-    // }
-
-    // if (searchItem in gamesSearchHistory) {
-    //   const list = { [searchItem]: gamesSearchHistory[searchItem] };
-    //   inputRef.current.disabled = false;
-    //   return dispatch(storeGameSearchList(list));
-    // }
-
-    dispatch(getListByName(searchItem))
-    inputRef.current.disabled = false;
+  const handleToggleChange = () => {
+    setisMoviesSearch(!isMoviesSearch);
   };
 
   const handleKeyDown = (event) => {
-    setSearchBoxClass("");
-    if (event.keyCode == 13) handleSubmit(true);
-  };
-
-  const handleFocus = () => {
-    setSearchBoxClass(classes.focused);
-  };
-
-  const handleType = (type) => {
-    setSearchType(type);
-    setSearchBoxClass("");
+    if (event.keyCode == 13) handleSubmit(event, true);
   };
 
   return (
-    <div className={classes.search_wrapper}>
-      <input
-        className={classes.search_input}
-        onKeyDown={handleKeyDown}
-        onFocus={handleFocus}
-        ref={inputRef}
-        type="search"
-        id="gamesSearch"
-        placeholder={INSCRIPTIONS_KEYS.SEARCH_BOX[searchType]}
-      />
-      <Link to="/results">
-        <span className={classes.search_lense} onClick={handleSubmit}>
-          <img className={classes.search_image} />
-        </span>
+    <div
+      className={`${classes.search_container} ${isMoviesSearch ? classes.movies : classes.games}`}
+    >
+      <Link
+        to="/results"
+        onClick={handleSubmit}
+        className={`${classes.lense_wrapper} ${isMoviesSearch ? classes.movies : classes.games}`}
+        title={isMoviesSearch ? "Search movies" : "Search games"}
+      >
+        <FaSearch className={classes.search_lens} />
       </Link>
-      <div className={`${classes.types} ${searchBoxClass}`}>
-        <div
-          onClick={() => handleType(SEARCH_TYPE.GAMES)}
-          className={`${classes.types_games} ${SEARCH_TYPE.GAMES === searchType ? classes.active : ""}`}
-        >
-          {INSCRIPTIONS_KEYS.SEARCH_BOX.GAMES}
-        </div>
-        <div
-          onClick={() => handleType(SEARCH_TYPE.FILMS)}
-          className={`${classes.types_films} ${SEARCH_TYPE.FILMS === searchType ? classes.active : ""}`}
-        >
-          {INSCRIPTIONS_KEYS.SEARCH_BOX.FILMS}
-        </div>
+      <div
+        className={`${classes.icon_wrapper} ${isMoviesSearch ? classes.movies : classes.games}`}
+        onClick={handleToggleChange}
+        title={isMoviesSearch ? "Switch to games" : "Switch to movies"}
+      >
+        {isMoviesSearch ? <FaFilm /> : <FaGamepad />}
       </div>
+      <input
+        onKeyDown={handleKeyDown}
+        ref={inputRef}
+        type="text"
+        placeholder={isMoviesSearch ? "Search movies..." : "Search games..."}
+        className={classes.search_input}
+      />
     </div>
   );
 }
