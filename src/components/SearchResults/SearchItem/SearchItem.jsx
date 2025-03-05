55ba8abca49c761354c0076dfc36ea759bae3037
Alex Kulik <bubralubra@gmail.com>
2025-03-05 20:33:44 +0300

    refactor: search logic

--- src/components/SearchResults/SearchItem/SearchItem.jsx
+++ src/components/SearchResults/SearchItem/SearchItem.jsx
@@ -1,14 +1,12 @@
 import { useDispatch, useSelector } from "react-redux";
 import classes from "./SearchItem.module.scss";
 import { Link } from "react-router-dom";
-import { selectDetails, selectSearchResultType } from "../../../selectors/selectors";
+import { selectSearchResultType } from "../../../selectors/selectors";
 import { ROUTES, SEARCH_TYPE } from "../../../constants/constants";
-import { setItemDetails } from "../../../actions";
 import { setDetails } from "../../../reducers/detailsReducer";
 
 export default function SearchItem({ data }) {
-  // if (!data) return;
-  const { logoUrl, name, enName, release, inCollection, rating, id } = data;
+  const { imageUrl, name, enName, release, inCollection, rating, id } = data;
   const itemType = useSelector(selectSearchResultType);
   const route =
     itemType === SEARCH_TYPE.GAMES
@@ -16,27 +14,26 @@ export default function SearchItem({ data }) {
       : ROUTES.CARDS.FILM + "?" + id;
   const dispatch = useDispatch();
   const handleClick = () => {
-    console.log('click');
     dispatch(setDetails(data));
   };
 
   return (
     <Link to={route}>
-      <div onClick={handleClick} className={classes.searchResults__item}>
-        <div className={classes.searchResults__item_poster}>
-          <img src={logoUrl} alt="" />
+      <div onClick={handleClick} className={classes.item}>
+        <div className={`${classes.poster} ${"canGhosted"}`}>
+          <img src={imageUrl} alt="" />
         </div>
-        <div className={classes.searchResults__item_content}>
+        <div className={classes.content}>
           <div className={classes.title}>
             <div className={"canGhosted"}>{name}</div>
             <div className={`${classes.entitle} ${"canGhosted"}`}>{enName}</div>
           </div>
         </div>
-        <div className={classes.searchResults__item_rating}>{rating || ""}</div>
-        <div className={`${classes.searchResults__item_released} ${"canGhosted"}`}>
+        <div className={classes.rating}>{rating || ""}</div>
+        <div className={`${classes.released} ${"canGhosted"}`}>
           {release || "In Development"}
         </div>
-        <div className={classes.searchResults__item_collection}>
+        <div className={classes.collection}>
           {inCollection ? "In Collection" : ""}
         </div>
       </div>
