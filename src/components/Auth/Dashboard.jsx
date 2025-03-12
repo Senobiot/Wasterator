8a1bad827a2539bd9719b3173d69f7a77c721bad
Alex Kulik <bubralubra@gmail.com>
2025-03-12 23:30:43 +0300

    refactor: added dasboard avatar field

--- src/components/Auth/Dashboard.jsx
+++ src/components/Auth/Dashboard.jsx
@@ -1,16 +1,25 @@
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { selectCurrentUser } from "../../selectors/selectors";
 import { logOff } from "../../reducers/authReducer";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import { ROUTES } from "../../constants/constants";
-import styles from "./Dashboard.module.scss"; // Import the CSS module
+import styles from "./Dashboard.module.scss";
 
 const Dashboard = () => {
+  const [avatar, setAvatar] = useState(null);
+  const [preview, setPreview] = useState(null);
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const loggedUser = useSelector(selectCurrentUser);
   const fields = Object.entries(loggedUser || {});
+
+  const handleAvatarChange = (event) => {
+    const file = event.target.files[0];
+    setAvatar(file);
+    setPreview(URL.createObjectURL(file));
+  };
 
   const handleClick = () => dispatch(logOff());
 
@@ -20,9 +29,26 @@ const Dashboard = () => {
     }
   }, [loggedUser, navigate]);
 
-  return !loggedUser ? null : (
+  return !loggedUser ? (
+    <div>Загрузка данных...</div>
+  ) : (
     <div className={styles.container}>
       <h2 className={styles.title}>Добро пожаловать, {loggedUser.name}!</h2>
+
+      <div className={styles.avatar}>
+        <img
+          src={preview || loggedUser.avatar || "default-avatar.png"}
+          alt="Avatar"
+          className={styles.avatarImage}
+        />
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleAvatarChange}
+          className={styles.avatarInput}
+        />
+      </div>
+
       <div className={styles.fields}>
         {fields.map(([key, value]) => (
           <div key={key} className={styles.field}>
